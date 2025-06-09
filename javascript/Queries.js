beea2c177469fc9d51e3d5bc1cbb266fcc3bea43
ikennot <domdomkenneth23@gmail.com>
2025-06-09 19:35:18 +0800

    Creating ui the buttons and timer

--- javascript/Queries.js
+++ javascript/Queries.js
@@ -2,11 +2,12 @@
 let alarmSound = document.querySelector('.alarmSound');
 document.querySelector('.timer-js').textContent = `${String(minutes).padStart(2,'0')}:${String(seconds).padStart(2,'0')}`;
 document.title =  `${String(minutes).padStart(2,'0')}:${String(seconds).padStart(2,'0')} - ${strTitle(pomodoroCount)}`;
-
+document.querySelector('.session-info-js').textContent =`${strTitle(pomodoroCount)}`;
 let session = document.querySelector('.session');
 session.textContent=`#${pomodoroCount}`;
 
 function displayTime(){
     document.querySelector('.timer-js').textContent = `${String(minutes).padStart(2,'0')}:${String(seconds).padStart(2,'0')}`;
+    document.querySelector('.session-info-js').textContent =`${strTitle(pomodoroCount)}`;
        document.title = `${String(minutes).padStart(2,'0')}:${String(seconds).padStart(2,'0')} - ${strTitle(pomodoroCount)}`;   
 }
